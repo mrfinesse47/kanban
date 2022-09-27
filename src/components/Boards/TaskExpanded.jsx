81efb04795400b006adb1824eb676b805e1c8e4f
Kevin Mason <krmason2020@gmail.com>
2022-09-26 17:42:46 -0700

    begin work on styling expanded

--- src/components/Boards/TaskExpanded.jsx
+++ src/components/Boards/TaskExpanded.jsx
@@ -10,7 +10,25 @@ const TaskExpanded = ({ task }) => {
       </header>
       <main>
         <p>{task.description}</p>
-        <div className='subtasks'></div>
+        <div className='subtasks'>
+          <h4>
+            {' '}
+            Subtasks (
+            {
+              task.subtasks.filter((subtask) => subtask.isCompleted === true)
+                .length
+            }{' '}
+            of {task.subtasks.length})
+          </h4>
+          <ul>
+            {task.subtasks.map((subtask) => (
+              <li>
+                <input type='checkbox' />
+                {subtask.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
       <footer></footer>
     </StyledTaskExpanded>
