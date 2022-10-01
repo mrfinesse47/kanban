22ae3ca36e573cd8f1d7aa99b6c02ab5d0f4a92c
Kevin Mason <krmason2020@gmail.com>
2022-09-30 17:32:26 -0700

    begin edit form

--- src/components/forms/EditTask.jsx
+++ src/components/forms/EditTask.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { StyledForm } from './styles/Form.styled';
 import { motion } from 'framer-motion';
+import { v4 as uuid } from 'uuid';
+import { useSelector } from 'react-redux';
 import DynamicList from './DynamicList';
 
 const EditTask = () => {
+  const { boards, selectedIndex, modalTask } = useSelector(
+    (state) => state.board
+  );
+
+  const subTasks = modalTask.subtasks.map((subtask) => ({
+    name: subtask.title,
+    error: { status: false, message: '' },
+    id: uuid(),
+  }));
+  console.log(subTasks);
+  const handleSubTaskChange = () => {
+    console.log('handle item change');
+  };
+  const handleSubTaskDelete = () => {
+    console.log('handle item delete');
+  };
+  const setErrorMessageOnSubTaskFalse = () => {
+    console.log('setErrorMessageOnItemFalse');
+  };
+  const handleAddSubTask = () => {
+    console.log('handleAdditem');
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -27,6 +52,14 @@ recharge the batteries a little.'
           />
         </div>
         {/* need to turn input list into component see in add new board */}
+        <DynamicList
+          title={'Subtasks'}
+          items={subTasks}
+          handleItemChange={handleSubTaskChange}
+          handleItemDelete={handleSubTaskDelete}
+          setErrorMessageOnItemFalse={setErrorMessageOnSubTaskFalse}
+          handleAdditem={handleAddSubTask}
+        />
       </StyledForm>
     </motion.div>
   );
